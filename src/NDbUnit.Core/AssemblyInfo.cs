71b46f9e862a4d8b6b7bbf94bb45f748c49271ad
Francis Garnier <francis.garnier@ne.ch>
2018-02-07 14:04:42 +0100

    Modification du numéro de version, à 4 chiffres pour éviter les problèmes de version avec les dll

--- src/NDbUnit.Core/AssemblyInfo.cs
+++ src/NDbUnit.Core/AssemblyInfo.cs
@@ -46,8 +46,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.6.8.*")]
-[assembly: AssemblyFileVersion("1.6.8.3")]
+[assembly: AssemblyVersion("1.6.8.4")]
+[assembly: AssemblyFileVersion("1.6.8.4")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
